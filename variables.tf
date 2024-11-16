0cd476578650dbb98f91f0039ad65d254f8e8b41
stefanvarnes <stefan_w24@hotmail.com>
2024-11-16 21:59:30 +0100

    var changes

--- variables.tf
+++ variables.tf
@@ -1,12 +1,13 @@
 variable "ARM_SUBSCRIPTION_ID" {
   type = string
   description = "ARM_SUBSCRIPTION_ID"
+  default = "secrets.ARM_CLIENT_ID"
 }
 
 variable "rg_base_name" {
   type        = string
   description = "The basename for resources and resourcegroups"
-  default     = "rg-tf-demo-5"
+  default     = "rg-tfdemo-modul7-sv"
 }
 
 variable "location" {
@@ -18,15 +19,17 @@ variable "location" {
 variable "saname" {
   type        = string
   description = "Storage Account name"
-  default     = "tfdemomodul8"
+  default     = "satfbackendsv"
 }
 
 variable "index_document" {
   type = string
   description = "index_document"
+  default = "index.html"
 }
 
 variable "source_content" {
   type = string
   description = "source content"
+  default = "<h1>Web page created with Terraform</h1>"
 }
